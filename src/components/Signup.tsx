16b571d6d38b09193213df5ca8afeec5df65a7f4
DejFcoder <trebatickydavid670@gmail.com>
2024-06-21 16:30:31 +0200

    fdfdfs

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -162,7 +162,7 @@ const Signup: React.FC = () => {
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Already have an account?{" "}
                 <Link
-                  to="/login"
+                  to="/"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   Login here
